c34b7e02b55e4e1d755d1b42d82f5abffcba672d
MURILO HENRIQUE ALVES CORREA <1090482221009@fatec-srv.local>
2024-10-30 08:42:52 -0300

    att

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,80 +1,43 @@
-import { z } from 'zod'
-import { cpf } from 'cpf-cnpj-validator'
+import dotenv from 'dotenv'
+dotenv.config() // Carrega as variáveis de ambiente do arquivo .env
 
-/*
-  O cliente deve ser maior de 18 anos.
-  Por isso, para validar a data de nascimento, calculamos
-  a data máxima em que o cliente pode ter nascido para ter
-  18 anos na data de hoje
-*/
-const maxBirthDate = new Date()   // Data de hoje
-maxBirthDate.setFullYear(maxBirthDate.getFullYear() - 18)
+import express, { json, urlencoded } from 'express'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
 
-// O cliente pode ter nascido, no máximo, há 120 anos
-const minBirthDate = new Date()
-minBirthDate.setFullYear(minBirthDate.getFullYear() - 120)
+import indexRouter from './routes/index.js'
+//import usersRouter from './routes/users.js'
 
-const Customer = z.object({
-  name:
-    z.string()
-    .min(5, { message: 'O nome deve ter, no mínimo, 5 caracteres' })
-    .includes(' ', { message: 'O nome deve ter um espaço em branco separando nome e sobrenome' }),
+const app = express()
 
-  ident_document:
-    z.string()
-    // Remove eventuais sublinhados se o usuário não preencheu
-    // totalmente o CPF
-    .transform(val => val.replace('_', ''))
-    .refine(val => val.length === 14, { message: 'O CPF deve ter exatamente 14 dígitos' })
-    .refine(val => cpf.isValid(val), { message: 'CPF inválido' }),
+import cors from 'cors'
 
-  birth_date:
-    // coerce força a conversão para o tipo Date, se o valor recebido for string
-    z.coerce.date()
-    .min(minBirthDate, { message: 'Data de nascimento está muito no passado' })
-    .max(maxBirthDate, { message: 'O cliente deve ser maior de 18 anos' })
-    .nullable(),     // O campo é opcional
+app.use(cors({
+  origin: process.env.FRONT_END_URL.split(','),
+  credentials: true   // Grava cookie no front-end
+}))
 
-  street_name:
-    z.string()
-    .max(40, { message: 'O logradoudo pode ter, no máximo, 40 caracteres' }),
+app.use(logger('dev'))
+app.use(json())
+app.use(urlencoded({ extended: false }))
+app.use(cookieParser())
 
-  house_number:
-    z.string()
-    .max(10, { message: 'O número pode ter, no máximo, 10 caracteres' }),
+app.use('/', indexRouter)
+//app.use('/users', usersRouter)
 
-  complements:
-    z.string()
-    .max(20, { message: 'O complemento pode ter, no máximo, 20 caracteres' })
-    .nullable(),
+/*********** ROTAS DA API **************/
 
-  district:
-    z.string()
-    .max(30, { message: 'O bairro pode ter, no máximo, 30 caracteres' }),
+// Middleware de verificação de autorização
+import authMiddleware from './middleware/auth.js'
+//app.use(authMiddleware)
 
-  municipality:
-    z.string()
-    .max(40, { message: 'O município pode ter, no máximo, 40 caracteres' }),
+import carsRouter from './routes/cars.js'
+app.use('/cars', carsRouter)
 
-  state:
-    z.string()
-    .length(2, { message: 'UF deve ter, exatamente, 2 caracteres' }),
+import customersRouter from './routes/customers.js'
+app.use('/customers', customersRouter)
 
-  phone:
-    z.string()
-    .transform(val => val.replace('_', ''))
-    // Depois de um transform(), não podemos usar length(). Por isso, devemos
-    // usar uma função personalizada com refine() para validar o tamanho do
-    // campo
-    .refine(val => {
-      console.log('val.length', val.length, 'val', val)
-      return val.length === 15
-}   , { message: 'O número do telefone/celular deve ter 15 posições' }),
+import usersRouter from './routes/users.js'
+app.use('/users', usersRouter)
 
-  email:
-    z.string()
-    .email({ message: 'E-mail inválido' })
-
-})
-
-export default Customer
+export default app
